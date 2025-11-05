194bf4fec403e3faa32b64dca27d11fdadab75e3
Zhengqi Gao <zhengqi@mit.edu>
2025-11-05 11:11:34 -0500

    update

--- main_test_bench.py
+++ main_test_bench.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     device = torch.cuda.current_device()
 
     causal = True
-    softmax_scale = None
+    softmax_scale = 0.5
     
     dtype = torch.bfloat16
     cu_k_seqlens = torch.tensor([0, 32, 61, 100, 134, 157], dtype=torch.int32, device=device) # [0, 32, 64, 96, 128, 160]
