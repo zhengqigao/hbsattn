fa87b2db33706f4f55e32838f6c4d8a0d1f3d7c9
Zhengqi Gao <zhengqi@mit.edu>
2025-11-06 17:50:22 -0500

    update

--- main_test_bench.py
+++ main_test_bench.py
@@ -18,11 +18,11 @@ if __name__ == "__main__":
     
     dtype = torch.float32
     
-    cu_k_seqlens = torch.tensor([0,32, 61, 100], dtype=torch.int32, device=device) # [0, 32, 64, 96, 128, 160] # , 61, 100, 134, 157
+    cu_k_seqlens = torch.tensor([0,32, 61, 100, 134, 157], dtype=torch.int32, device=device) # [0, 32, 64, 96, 128, 160] # , 61, 100, 134, 157
     max_k_seqlen = int((cu_k_seqlens[1:] - cu_k_seqlens[:-1]).max().item())
     k_seqlen = cu_k_seqlens[-1].item()
     
-    cu_q_seqlens = torch.tensor([0, 32, 64, 96], dtype=torch.int32, device=device) # [0, 32, 64, 96, 128, 160]
+    cu_q_seqlens = torch.tensor([0, 32, 64, 96, 128, 160], dtype=torch.int32, device=device) # [0, 32, 64, 96, 128, 160]
     max_q_seqlen = int((cu_q_seqlens[1:] - cu_q_seqlens[:-1]).max().item())
     q_seqlen = cu_q_seqlens[-1].item()
     
