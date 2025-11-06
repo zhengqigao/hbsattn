345d7041af364a77609996f2dee4e020b7fb5ec4
Zhengqi Gao <zhengqi@mit.edu>
2025-11-06 09:57:03 -0500

    update

--- hbsattn/fwd_triton.py
+++ hbsattn/fwd_triton.py
@@ -122,8 +122,8 @@ def _fwd_kernel(
             # Triton device_print cannot print 'dtype' objects, only tensor values.
             # Instead, print the converted tensors (for debugging, use the value, not the type)
             tl.device_print("v_block", v_block)
-            acc += tl.dot(p, v_block)
-            # acc += tl.sum(v_block,0)
+            # acc += tl.dot(p, v_block)
+            acc += tl.sum(v_block,0)
             tl.device_print("sum ofacc", acc)
             m_i = m_ij
 
