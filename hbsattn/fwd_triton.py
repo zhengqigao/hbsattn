d808a7087b25d1bc7d8f986a66ec427dfbcad805
Zhengqi Gao <zhengqi@mit.edu>
2025-11-06 13:55:01 -0500

    update

--- hbsattn/fwd_triton.py
+++ hbsattn/fwd_triton.py
@@ -115,6 +115,7 @@ def _fwd_kernel(
             l_i = l_i * alpha + l_ij
             acc = acc * alpha[:, None]
             tl.device_print("p", p)
+            tl.device_print("v_block", v_block)
             p = p.to(v.type.element_ty)
             acc += tl.dot(p, v_block, allow_tf32=False)
 
