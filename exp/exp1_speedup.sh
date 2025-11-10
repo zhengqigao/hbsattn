ddfe8eacccf10cfa11b268981e812e18159ef286
Zhengqi Gao <zhengqi@mit.edu>
2025-11-10 13:34:35 -0500

    update

--- exp/exp1_speedup.sh
+++ exp/exp1_speedup.sh
@@ -1,13 +1,34 @@
 cd ..
 
+echo "Benchmarking with unit_seqlen 128"
 python test_speedup.py --unit_seqlen 128 --nruns 15 --nwarmup 5
+
+echo "Benchmarking with unit_seqlen 256"
 python test_speedup.py --unit_seqlen 256 --nruns 15 --nwarmup 5
+
+echo "Benchmarking with unit_seqlen 512"
 python test_speedup.py --unit_seqlen 512 --nruns 15 --nwarmup 5
+
+echo "Benchmarking with unit_seqlen 1024"
 python test_speedup.py --unit_seqlen 1024 --nruns 15 --nwarmup 5
+
+echo "Benchmarking with unit_seqlen 2048"
 python test_speedup.py --unit_seqlen 2048 --nruns 15 --nwarmup 5
+
+echo "Benchmarking with unit_seqlen 4096"
 python test_speedup.py --unit_seqlen 4096 --nruns 15 --nwarmup 5
+
+echo "Benchmarking with unit_seqlen 8192"
 python test_speedup.py --unit_seqlen 8192 --nruns 15 --nwarmup 5
+
+echo "Benchmarking with unit_seqlen 16384"
 python test_speedup.py --unit_seqlen 16384 --nruns 15 --nwarmup 5
+
+echo "Benchmarking with unit_seqlen 32768"
 python test_speedup.py --unit_seqlen 32768 --nruns 15 --nwarmup 5
+
+echo "Benchmarking with unit_seqlen 65536"
 python test_speedup.py --unit_seqlen 65536 --nruns 15 --nwarmup 5
+
+echo "Benchmarking with unit_seqlen 131072"
 python test_speedup.py --unit_seqlen 131072 --nruns 15 --nwarmup 5
