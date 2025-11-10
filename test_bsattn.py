3fabf8c9d8ecc98eac5ad02774ddb8b0e0f512a7
Zhengqi Gao <zhengqi@mit.edu>
2025-11-10 09:45:17 -0500

    update

--- test_bsattn.py
+++ test_bsattn.py
@@ -7,12 +7,58 @@ from block_sparse_attn import (
     flash_attn_varlen_func,
 )
 
-from utils import (
-    time_fwd,
-    flops,
-    efficiency,
-    write_to_excel,
-)
+# Adapted from https://github.com/Dao-AILab/flash-attention/blob/main/benchmarks/benchmark_flash_attention.py
+
+import openpyxl
+from block_sparse_attn.utils.benchmark import benchmark_forward
+import math
+import torch
+import os
+
+
+def benchmark_fwd(
+    fn,
+    *inputs,
+    grad=None,
+    repeats=10,
+    desc="",
+    verbose=True,
+    amp=False,
+    amp_dtype=torch.float16,
+    **kwinputs,
+):
+    """Use Pytorch Benchmark on the forward pass of an arbitrary function."""
+    return benchmark_forward(
+            fn,
+            *inputs,
+            repeats=repeats,
+            desc=desc,
+            verbose=verbose,
+            amp=amp,
+            amp_dtype=amp_dtype,
+            **kwinputs,
+            )
+
+def flops(batch, seqlen, headdim, nheads, causal, mode="fwd"):
+    assert mode in ["fwd"]
+    f = 4 * batch * seqlen**2 * nheads * headdim // (2 if causal else 1)
+    return f if mode == "fwd" else (2.5 * f if mode == "bwd" else 3.5 * f)
+
+def efficiency(flop, time):
+    return (flop / time / 10**12) if not math.isnan(time) else 0.0
+
+def time_fwd(func, *args, **kwargs):
+    time_f = benchmark_fwd(func, *args, **kwargs)
+    return time_f[1].mean
+
+def write_to_excel(label, data, dir_path, file_name):
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    sheet.append(label)
+    os.makedirs(dir_path, exist_ok=True)
+    for row in data:
+        sheet.append(row)
+    workbook.save(dir_path + file_name + ".xlsx")
 
 def generate_base_sparsity_mask(max_seqlen_q, max_seqlen_k, round_base, m_block_dim, n_block_dim, sparsity, causal=False, device="cuda"):
     def round_to_multiple(x, base):
