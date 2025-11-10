cffd45d8518552c0948b2af3d83d42e23eafdbb8
Zhengqi Gao <zhengqi@mit.edu>
2025-11-10 11:47:09 -0500

    update

---
+++ exp/exp1_speedup.sh
@@ -0,0 +1,10 @@
+cd ..
+
+python test_speedup.py --unit_seqlen 1024 --nruns 15 --nwarmup 5
+python test_speedup.py --unit_seqlen 2048 --nruns 15 --nwarmup 5
+python test_speedup.py --unit_seqlen 4096 --nruns 15 --nwarmup 5
+python test_speedup.py --unit_seqlen 8192 --nruns 15 --nwarmup 5
+python test_speedup.py --unit_seqlen 16384 --nruns 15 --nwarmup 5
+python test_speedup.py --unit_seqlen 32768 --nruns 15 --nwarmup 5
+python test_speedup.py --unit_seqlen 65536 --nruns 15 --nwarmup 5
+python test_speedup.py --unit_seqlen 131072 --nruns 15 --nwarmup 5
