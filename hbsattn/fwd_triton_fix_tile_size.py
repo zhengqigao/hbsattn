ed0f346c0b1a4cb848e4465156a8ad3d08583ac9
Zhengqi Gao <zhengqi@mit.edu>
2025-11-11 16:52:13 -0500

    update

--- hbsattn/fwd_triton_fix_tile_size.py
+++ hbsattn/fwd_triton_fix_tile_size.py
@@ -278,7 +278,7 @@ def _forward_fix_tile_size(q, k, v, cu_q_seqlens, cu_k_seqlens, block_mask, q_bl
     )
     
     best_cfg = _fwd_kernel.best_config
-    print(f"[Autotune Result] BLOCK_M={best_cfg.kwargs['BLOCK_M']}, BLOCK_N={best_cfg.kwargs['BLOCK_N']}, num_warps={best_cfg.num_warps}, num_stages={best_cfg.num_stages}")
+    print(f"[Autotune Result]num_warps={best_cfg.num_warps}, num_stages={best_cfg.num_stages}")
     
     return out
 
