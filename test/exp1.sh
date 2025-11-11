3c071a95aa944fa132afac25f427201cfe6730de
Zhengqi Gao <zhengqi@mit.edu>
2025-11-11 09:57:02 -0500

    update

---
+++ test/exp1.sh
@@ -0,0 +1,8 @@
+
+sbatch ./test/sbatch_speedup.sh 0.3 true
+sbatch ./test/sbatch_speedup.sh 0.5 true
+sbatch ./test/sbatch_speedup.sh 0.7 true
+
+sbatch ./test/sbatch_speedup.sh 0.3 false
+sbatch ./test/sbatch_speedup.sh 0.5 false
+sbatch ./test/sbatch_speedup.sh 0.7 false
