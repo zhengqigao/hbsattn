7e54656a8642cf1dab0475c3002b9a7bf388fd4f
Zhengqi Gao <zhengqi@mit.edu>
2025-11-11 19:11:32 -0500

    update

--- test/exp1.sh
+++ test/exp1.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-sbatch ./test/sbatch_speedup.sh 0.3 true
-sbatch ./test/sbatch_speedup.sh 0.5 true
-sbatch ./test/sbatch_speedup.sh 0.7 true
+# sbatch ./test/sbatch_speedup.sh 0.3 true
+# sbatch ./test/sbatch_speedup.sh 0.5 true
+# sbatch ./test/sbatch_speedup.sh 0.7 true
 
-sbatch ./test/sbatch_speedup.sh 0.3 false
-sbatch ./test/sbatch_speedup.sh 0.5 false
-sbatch ./test/sbatch_speedup.sh 0.7 false
+# sbatch ./test/sbatch_speedup.sh 0.3 false
+# sbatch ./test/sbatch_speedup.sh 0.5 false
+# sbatch ./test/sbatch_speedup.sh 0.7 false
+
+./test/bash_speedup.sh 0.3 true
+./test/bash_speedup.sh 0.4 true
+./test/bash_speedup.sh 0.5 true
+./test/bash_speedup.sh 0.6 true
+./test/bash_speedup.sh 0.7 true
+./test/bash_speedup.sh 0.8 true
+./test/bash_speedup.sh 0.9 true
+
+
+./test/bash_speedup.sh 0.3 false
+./test/bash_speedup.sh 0.4 false
+./test/bash_speedup.sh 0.5 false
+./test/bash_speedup.sh 0.6 false
+./test/bash_speedup.sh 0.7 false
+./test/bash_speedup.sh 0.8 false
+./test/bash_speedup.sh 0.9 false
