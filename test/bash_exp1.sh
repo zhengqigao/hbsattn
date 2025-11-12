91e47fb9811460800c2e7ee70cb8c30640e17882
Zhengqi Gao <zhengqi@mit.edu>
2025-11-11 19:12:32 -0500

    update

--- test/bash_exp1.sh
+++ test/bash_exp1.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# sbatch ./test/sbatch_speedup.sh 0.3 true
-# sbatch ./test/sbatch_speedup.sh 0.5 true
-# sbatch ./test/sbatch_speedup.sh 0.7 true
-
-# sbatch ./test/sbatch_speedup.sh 0.3 false
-# sbatch ./test/sbatch_speedup.sh 0.5 false
-# sbatch ./test/sbatch_speedup.sh 0.7 false
 
 ./test/bash_speedup.sh 0.3 true
 ./test/bash_speedup.sh 0.4 true
