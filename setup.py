64ba31fc079adf89406cdb90a4b9c8a1ac9cc0ad
Zhengqi Gao <zhengqi@mit.edu>
2025-11-12 09:22:16 -0500

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hbsattn",
-    version="0.1.0",
+    version="0.0.1",
     description="Block-sparse attention with heterogenoues block size",
     author="Zhengqi Gao",
     author_email="zhengqi@mit.edu",
