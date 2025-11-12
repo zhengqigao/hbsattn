d8eb1b65f00e3100bfe8bb1a737c2866a67ed74c
Zhengqi Gao <zhengqi@mit.edu>
2025-11-11 19:18:03 -0500

    update

--- test/bash_exp2.sh
+++ test/bash_exp2.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
 
-# ./test/bash_speedup.sh 0.3 true
-# ./test/bash_speedup.sh 0.4 true
-# ./test/bash_speedup.sh 0.5 true
-# ./test/bash_speedup.sh 0.6 true
-# ./test/bash_speedup.sh 0.7 true
-# ./test/bash_speedup.sh 0.8 true
-# ./test/bash_speedup.sh 0.9 true
 
 
-./test/bash_speedup.sh 0.3 false
-./test/bash_speedup.sh 0.4 false
-./test/bash_speedup.sh 0.5 false
-./test/bash_speedup.sh 0.6 false
-./test/bash_speedup.sh 0.7 false
-./test/bash_speedup.sh 0.8 false
+
 ./test/bash_speedup.sh 0.9 false
+./test/bash_speedup.sh 0.8 false
+./test/bash_speedup.sh 0.7 false
+./test/bash_speedup.sh 0.6 false
+./test/bash_speedup.sh 0.5 false
+./test/bash_speedup.sh 0.4 false
+./test/bash_speedup.sh 0.3 false
