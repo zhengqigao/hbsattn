50795a871f5836d03a633283094e9e9604505e0d
Zhengqi Gao <zhengqi@mit.edu>
2025-11-13 16:32:46 -0500

    update

--- hbsattn/fwd_triton_scheduling.py
+++ hbsattn/fwd_triton_scheduling.py
@@ -303,7 +303,7 @@ def _forward_scheduling(q, k, v, cu_q_seqlens, cu_k_seqlens, block_mask, q_block
     start_time = time.time()
     num_q_group, cu_num_q_group, q_group_to_batch, q_assignment, k_assignment = _scheduling(block_mask, cu_num_q_block, batch_size, schedule_func, num_block_per_group)
     end_time = time.time()
-    print(f"scheduling time: {end_time - start_time:.3f} sec")
+    print(f"scheduling time: {end_time - start_time:.3e} sec")
     # print(f"num_block_per_group: {num_block_per_group}, num_q_group: {num_q_group}, cu_num_q_group: {cu_num_q_group}, q_group_to_batch: {q_group_to_batch}, q_assignment: {q_assignment}, k_assignment: {k_assignment}")
     # launch kernel
     grid = (num_q_group, nhead_q)
